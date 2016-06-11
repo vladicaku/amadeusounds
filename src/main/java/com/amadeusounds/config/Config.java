79475e65ca5dc1828263b81f73881cdeebb984bc
Vladica Jovanovski <vladica.jovanovski@gmail.com>
2016-06-11 16:51:35 +0200

    setMaxFileSize increased

--- src/main/java/com/amadeusounds/config/Config.java
+++ src/main/java/com/amadeusounds/config/Config.java
@@ -1,9 +1,12 @@
 package com.amadeusounds.config;
 
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.servlet.MultipartConfigElement;
 
 /**
  * Created by Vac on 4/29/2016.
@@ -13,5 +16,13 @@ public class Config {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        factory.setMaxFileSize("5120MB");
+        factory.setMaxRequestSize("5120MB");
+        return factory.createMultipartConfig();
     }
 }
