eef16bd30734c0b89cca424487881c068cad2733
Vladica Jovanovski <vladica.jovanovski@gmail.com>
2016-06-12 15:29:00 +0200

    Search implemented

--- src/main/resources/static/old/app/home/homeService.js
+++ src/main/resources/static/old/app/home/homeService.js
@@ -5,6 +5,15 @@ amadeusounds.factory('HomeService', ['$http', function ($http) {
     var songsUrl = 'http://localhost:8080/api/songs/';
     var service = {};
 
+    service.searchSongs = function(pagination, term) {
+        var url = songsUrl + '/search/' + term + '?page=' + (pagination.current - 1) + '&size=' + pagination.itemsPerPage;
+        console.log(url);
+        return $http({
+            method: 'GET',
+            url: url
+        });
+    };
+
     service.getLatestSongs = function(pagination) {
         var url = songsUrl + '/latest?page=' + (pagination.current - 1) + '&size=' + pagination.itemsPerPage;
         console.log(url);
--- src/main/resources/static/old/app/menu/menuSetup.js
+++ src/main/resources/static/old/app/menu/menuSetup.js
@@ -5,6 +5,10 @@ amadeusounds.run(['$rootScope', '$state', 'CategoriesService', 'TagsService',
     function ($rootScope, $state, CategoriesService, TagsService) {
         console.log("Menu setup");
 
+        $rootScope.search = function() {
+            $state.go("home", {arg1: 'search'}, { reload: true });
+        };
+
         CategoriesService.getAllCategories().then(function (response) {
             $rootScope.categories = response.data;
         });
